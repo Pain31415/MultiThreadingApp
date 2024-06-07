287c6ac6604e947906ef89d826c14370226d137c
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-06-07 19:16:25 +0300

    Add project files.

---
+++ MultiThreadingApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MultiThreadingApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
